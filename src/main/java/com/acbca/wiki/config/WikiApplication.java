2d979af0e215e6c7f6bc01aa5e453003b0e52597
acbca <idacbca@126.com>
2022-02-09 15:48:12 -0500

    2-5 Added hello world interface Address: http://127.0.0.1:8880/hello

--- src/main/java/com/acbca/wiki/config/WikiApplication.java
+++ src/main/java/com/acbca/wiki/config/WikiApplication.java
@@ -1,11 +1,14 @@
-package com.acbca.wiki;
+package com.acbca.wiki.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
 
+// @ComponentScan({"com.acbca", "com.test"})
+@ComponentScan("com.acbca")
 @SpringBootApplication
 public class WikiApplication {
 
