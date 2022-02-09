4396a6028ab3031291995b1ab4a918497c207c28
acbca <idacbca@126.com>
2022-02-09 16:26:06 -0500

    2-8 Added a custom configuration example

--- src/main/java/com/acbca/wiki/controller/TestController.java
+++ src/main/java/com/acbca/wiki/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.acbca.wiki.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,13 +8,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
+    @Value("${test.hello:TEST}")
+    private String testHello;
+
     /**
      * GET, POST, PUT, DELETE
      * @return
      */
     @GetMapping("/hello")
     public String hello() {
-        return "Hello World!";
+        return "Hello World!" + testHello;
     }
 
     @PostMapping("/hello/post")
