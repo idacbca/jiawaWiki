f201b4adc551b5116f28abe3c14566e3981b1982
acbca <idacbca@126.com>
2022-02-09 16:12:25 -0500

    2-7 Added a http test (test.http) for the hello interface

--- src/main/java/com/acbca/wiki/controller/TestController.java
+++ src/main/java/com/acbca/wiki/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.acbca.wiki.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,6 +13,11 @@ public class TestController {
      */
     @GetMapping("/hello")
     public String hello() {
-        return "Hello, World!";
+        return "Hello World!";
+    }
+
+    @PostMapping("/hello/post")
+    public String helloPost(String name) {
+        return "Hello World! Post, " + name;
     }
 }
