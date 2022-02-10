62cbc1ba6b03e7295ed27099ebf82e298033dd6e
acbca <idacbca@126.com>
2022-02-10 15:10:59 -0500

    3-5 Added a test query for demo table

---
+++ src/main/java/com/acbca/wiki/service/DemoService.java
@@ -0,0 +1,19 @@
+package com.acbca.wiki.service;
+
+import com.acbca.wiki.domain.Demo;
+import com.acbca.wiki.mapper.DemoMapper;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
+public class DemoService {
+
+    @Resource
+    private DemoMapper demoMapper;
+
+    public List<Demo> list() {
+        return demoMapper.selectByExample(null);
+    }
+}
