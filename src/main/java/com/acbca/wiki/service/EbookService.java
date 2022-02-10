83cb06a62d4df23204c5adc9ff5c5cb49bdc367c
acbca <idacbca@126.com>
2022-02-10 16:00:17 -0500

    3-6 Completed ebook table query interface

---
+++ src/main/java/com/acbca/wiki/service/EbookService.java
@@ -0,0 +1,19 @@
+package com.acbca.wiki.service;
+
+import com.acbca.wiki.domain.Ebook;
+import com.acbca.wiki.mapper.EbookMapper;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
+public class EbookService {
+
+    @Resource
+    private EbookMapper ebookMapper;
+
+    public List<Ebook> list() {
+        return ebookMapper.selectByExample(null);
+    }
+}
