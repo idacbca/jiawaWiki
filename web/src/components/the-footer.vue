689a61fc0730fc9a83b22c16981aca8c6a2da680
acbca <idacbca@126.com>
2022-02-12 18:42:42 -0500

    4-6 Extract the footer to the-footer component.

---
+++ web/src/components/the-footer.vue
@@ -0,0 +1,13 @@
+<template>
+  <a-layout-footer style="text-align: center">
+    Wiki电子书 ©2022 Created by acbca
+  </a-layout-footer>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'the-footer'
+});
+</script>
