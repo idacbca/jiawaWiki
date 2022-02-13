a53c9d2a2b59a9c032890e8fd4af411e42755fa6
acbca <idacbca@126.com>
2022-02-13 02:15:38 -0500

    5-4 Modified EbookService to determine if parameters are passed in, and adjusted the cover icon's style.

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -109,7 +109,7 @@ export default defineComponent({
 
     onMounted(() => {
       console.log("onMounted");
-      axios.get("http://localhost:8880/ebook/list?name=Spring").then((response) => {
+      axios.get("http://localhost:8880/ebook/list").then((response) => {
         const data = response.data;
         ebooks.value = data.content;
         ebooks1.books = data.content;
@@ -136,3 +136,13 @@ export default defineComponent({
   }
 });
 </script>
+
+<style scoped>
+.ant-avatar {
+  width: 50px;
+  height: 50px;
+  line-height: 50px;
+  border-radius: 8%;
+  margin: 5px 0;
+}
+</style>
