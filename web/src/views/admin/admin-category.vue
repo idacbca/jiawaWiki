860afaa2c76e1ccec2f6a14210f3daf18cc653a9
acbca <idacbca@126.com>
2022-02-16 18:16:32 -0500

    7-4 Display category table in tree structure. Added a helper function to transform array to tree structure data.

--- web/src/views/admin/admin-category.vue
+++ web/src/views/admin/admin-category.vue
@@ -20,7 +20,7 @@
       <a-table
           :columns="columns"
           :row-key="record => record.id"
-          :data-source="categorys"
+          :data-source="level1"
           :loading="loading"
           :pagination="false"
       >
@@ -101,6 +101,20 @@ export default defineComponent({
         slots: { customRender: 'action' }
       }
     ];
+
+    /**
+     * 一级分类树，children属性就是二级分类
+     * [{
+     *   id: "",
+     *   name: "",
+     *   children: [{
+     *     id: "",
+     *     name: "",
+     *   }]
+     * }]
+     */
+    const level1 = ref(); // 一级分类树，children属性就是二级分类
+
     /**
      * 数据查询
      **/
@@ -112,6 +126,10 @@ export default defineComponent({
         const data = response.data;
         if (data.success) {
           categorys.value = data.content;
+          console.log("原始数组：", categorys.value);
+          level1.value = [];
+          level1.value = Tool.array2Tree(categorys.value, 0);
+          console.log("树形结构：", level1);
         } else {
           message.error(data.message);
         }
@@ -167,7 +185,8 @@ export default defineComponent({
     });
     return {
       param,
-      categorys,
+      // categorys,
+      level1,
       columns,
       loading,
       handleQuery,
