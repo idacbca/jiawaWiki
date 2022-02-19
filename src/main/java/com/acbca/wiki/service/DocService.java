6863b3f7cce4656a7ae0488456797b3641409891
acbca <idacbca@126.com>
2022-02-19 17:52:42 -0500

    8-2 Added a new doc table, and generated persistent level code using mybatis generator.

---
+++ src/main/java/com/acbca/wiki/service/DocService.java
@@ -0,0 +1,92 @@
+package com.acbca.wiki.service;
+
+import com.acbca.wiki.domain.Doc;
+import com.acbca.wiki.domain.DocExample;
+import com.acbca.wiki.mapper.DocMapper;
+import com.acbca.wiki.req.DocQueryReq;
+import com.acbca.wiki.req.DocSaveReq;
+import com.acbca.wiki.resp.DocQueryResp;
+import com.acbca.wiki.resp.PageResp;
+import com.acbca.wiki.util.CopyUtil;
+import com.acbca.wiki.util.SnowFlake;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
+public class DocService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(DocService.class);
+
+    @Resource
+    private DocMapper docMapper;
+
+    @Resource
+    private SnowFlake snowFlake = new SnowFlake(1, 1);
+
+    public PageResp<DocQueryResp> list(DocQueryReq req) {
+        DocExample docExample = new DocExample();
+        docExample.setOrderByClause("sort asc");
+        DocExample.Criteria criteria = docExample.createCriteria();
+        PageHelper.startPage(req.getPage(), req.getSize());
+        List<Doc> docList = docMapper.selectByExample(docExample);
+
+        PageInfo<Doc> pageInfo = new PageInfo<>(docList);
+        LOG.info("总行数：{}", pageInfo.getTotal());
+        LOG.info("总页数：{}", pageInfo.getPages());
+
+//        List<DocResp> respList = new ArrayList<>();
+//        for (Doc doc : docList) {
+////            DocResp docResp = new DocResp();
+////            BeanUtils.copyProperties(doc, docResp);
+//            DocResp docResp = CopyUtil.copy(doc, DocResp.class);
+//            respList.add(docResp);
+//        }
+
+        List<DocQueryResp> respList = CopyUtil.copyList(docList, DocQueryResp.class);
+
+        PageResp<DocQueryResp> pageResp = new PageResp<>();
+        pageResp.setTotal(pageInfo.getTotal());
+        pageResp.setList(respList);
+
+        return pageResp;
+    }
+
+    public List<DocQueryResp> all() {
+        DocExample docExample = new DocExample();
+        docExample.setOrderByClause("sort asc");
+        List<Doc> docList = docMapper.selectByExample(docExample);
+
+        List<DocQueryResp> respList = CopyUtil.copyList(docList, DocQueryResp.class);
+
+        return respList;
+    }
+
+    /**
+     * 保存
+     */
+    public void save(DocSaveReq req) {
+        Doc doc = CopyUtil.copy(req, Doc.class);
+        if (ObjectUtils.isEmpty(req.getId())) {
+            // 新增
+            doc.setId(snowFlake.nextId());
+            docMapper.insert(doc);
+        } else {
+            // 更新
+            docMapper.updateByPrimaryKey(doc);
+        }
+    }
+
+    /**
+     * 删除
+     */
+    public void delete(Long id) {
+        docMapper.deleteByPrimaryKey(id);
+    }
+}
