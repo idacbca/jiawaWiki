cc4186420d61966efb211232b6cebb20fb481e88
acbca <idacbca@126.com>
2022-02-19 16:22:22 -0500

    7-7 Make handleQuery executes after handleQueryCategory

--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -268,10 +268,6 @@ export default defineComponent({
 
     onMounted(() => {
       handleQueryCategory();
-      handleQuery({
-        page: 1,
-        size: pagination.value.pageSize
-      });
     });
     return {
       param,
