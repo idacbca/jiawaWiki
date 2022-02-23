9c8287fd6ac2dad1d9092a51fa10b69e56147cd0
acbca <idacbca@126.com>
2022-02-23 02:43:59 -0500

    9-4 No username changes allowed during editing

--- src/main/java/com/acbca/wiki/service/UserService.java
+++ src/main/java/com/acbca/wiki/service/UserService.java
@@ -80,7 +80,8 @@ public class UserService {
             }
         } else {
             // 更新
-            userMapper.updateByPrimaryKey(user);
+            user.setLoginName(null);
+            userMapper.updateByPrimaryKeySelective(user);
         }
     }
 
