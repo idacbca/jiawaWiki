db8fae16363cfa713d2c70204965e139d5253c1e
acbca <idacbca@126.com>
2022-02-23 02:58:44 -0500

    9-5 Encrypted transmission and encrypted storage of passwords

--- web/src/views/admin/admin-user.vue
+++ web/src/views/admin/admin-user.vue
@@ -75,6 +75,10 @@ import { defineComponent, onMounted, ref } from 'vue';
 import axios from 'axios';
 import { message } from 'ant-design-vue';
 import {Tool} from "@/util/tool";
+
+declare let hexMd5: any;
+declare let KEY: any;
+
 export default defineComponent({
   name: 'AdminUser',
   setup() {
@@ -148,6 +152,9 @@ export default defineComponent({
     const modalLoading = ref(false);
     const handleModalOk = () => {
       modalLoading.value = true;
+
+      user.value.password = hexMd5(user.value.password + KEY);
+
       axios.post("/user/save", user.value).then((response) => {
         modalLoading.value = false;
         const data = response.data; // data = commonResp
