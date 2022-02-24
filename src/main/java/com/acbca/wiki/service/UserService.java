bdf454206eb5d65937a8aa0c932c214a62bd79c1
acbca <idacbca@126.com>
2022-02-23 19:24:54 -0500

    9-6 Can't modify password when edit

--- src/main/java/com/acbca/wiki/service/UserService.java
+++ src/main/java/com/acbca/wiki/service/UserService.java
@@ -81,6 +81,7 @@ public class UserService {
         } else {
             // 更新
             user.setLoginName(null);
+            user.setPassword(null);
             userMapper.updateByPrimaryKeySelective(user);
         }
     }
