f308e107ea0b84cc5683d0542145bf67477a9f44
acbca <idacbca@126.com>
2022-02-23 22:52:37 -0500

    9-9 Put login token into redis

--- src/main/java/com/acbca/wiki/controller/UserController.java
+++ src/main/java/com/acbca/wiki/controller/UserController.java
@@ -9,18 +9,32 @@ import com.acbca.wiki.resp.PageResp;
 import com.acbca.wiki.resp.UserLoginResp;
 import com.acbca.wiki.resp.UserQueryResp;
 import com.acbca.wiki.service.UserService;
+import com.acbca.wiki.util.SnowFlake;
+import com.alibaba.fastjson.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.util.DigestUtils;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import javax.validation.Valid;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
 
+    final Logger LOG = LoggerFactory.getLogger(UserController.class);
+
     @Resource
     private UserService userService;
+
+    @Resource
+    private SnowFlake snowFlake = new SnowFlake(1, 1);
+
+    @Resource
+    private RedisTemplate redisTemplate;
 
     @GetMapping("/list")
     public CommonResp list(@Valid UserQueryReq req) {
@@ -54,10 +68,16 @@ public class UserController {
     }
 
     @PostMapping("/login")
-    public CommonResp login(@Valid @RequestBody UserLoginReq req){
+    public CommonResp login(@Valid @RequestBody UserLoginReq req) {
         req.setPassword(DigestUtils.md5DigestAsHex(req.getPassword().getBytes()));
         CommonResp<UserLoginResp> resp = new CommonResp<>();
         UserLoginResp userLoginResp = userService.login(req);
+
+        Long token = snowFlake.nextId();
+        LOG.info("生成单点登录token：{}，并放入redis中", token);
+        userLoginResp.setToken(token.toString());
+        redisTemplate.opsForValue().set(token.toString(), JSONObject.toJSONString(userLoginResp), 3600 * 24, TimeUnit.SECONDS);
+
         resp.setContent(userLoginResp);
         return resp;
     }
